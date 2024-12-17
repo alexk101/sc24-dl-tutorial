eb2a7a8438a0a7471075e11eae2d334f6bc9e8fd
Alex Kiefer <alkiefer@iu.edu>
2024-12-16 21:59:46 -0500

    removed old files

--- test_model_dims.py
+++
@@ -1,8 +0,0 @@
-import torch
-from networks.vit import ViT
-from utils.YParams import YParams
-from torchinfo import summary
-
-params = YParams('./config/ViT.yaml', 'short_mp')
-model = ViT(params)
-summary(model, input_size=(16,20,360,720))
