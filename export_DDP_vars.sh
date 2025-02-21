dbaaf783c01da5071c558c062752a9ca1d2cbab2
alexk101 <akiefer3@utk.edu>
2025-02-21 12:54:53 -0500

    suggested changes

--- export_DDP_vars.sh
+++ export_DDP_vars.sh
@@ -2,5 +2,6 @@ export RANK=$SLURM_PROCID
 export LOCAL_RANK=$SLURM_LOCALID
 export WORLD_SIZE=$SLURM_NTASKS
 export MASTER_PORT=29500 # default from torch launcher
+# export MASTER_ADDR=$(hostname -i)
 # export MASTER_ADDR=$SLURM_SUBMIT_HOST
-# export MASTER_ADDR=$HOSTNAME
+export MASTER_ADDR=$HOSTNAME
