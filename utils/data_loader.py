bfc811a609957a860c3d326fb66cf15f320bcc98
alexk101 <akiefer3@utk.edu>
2025-02-23 03:11:54 -0500

    override stats location

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -9,6 +9,8 @@ from torch.utils.data.distributed import DistributedSampler
 from torch import Tensor
 import h5py
 
+DATA_DIR = os.getenv("DATADIR")
+
 def worker_init(wrk_id):
     np.random.seed(torch.utils.data.get_worker_info().seed%(2**32 - 1))
 
@@ -50,8 +52,8 @@ class ERA5Dataset(Dataset):
         self.n_in_channels = params.n_in_channels
         self.n_out_channels = params.n_out_channels
         self.normalize = True
-        self.means = np.load(params.global_means_path)[0]
-        self.stds = np.load(params.global_stds_path)[0]
+        self.means = np.load(f'{DATA_DIR}/stats/global_means.npy')[0]
+        self.stds = np.load(f'{DATA_DIR}/stats/global_stds.npy')[0]
         self.limit_nsamples = params.limit_nsamples if train else params.limit_nsamples_val
         self._get_files_stats()
 
