e10c24557602bc163d312d8f7bd1dacd3e0adb77
Alex Kiefer <alexkiefer51@gmail.com>
2025-02-28 15:19:28 -0500

    perhaps infiniband var

--- export_frontier_vars.sh
+++ export_frontier_vars.sh
@@ -5,3 +5,4 @@ export NCCL_SOCKET_FAMILY=ipv4
 export MPICH_GPU_SUPPORT_ENABLED=1
 export NCCL_CROSS_NIC=1       # On large systems, this NCCL setting has been found to improve perf
 export PMI_NO_FORK=1
+export NCCL_IB_HCA=hsn0
