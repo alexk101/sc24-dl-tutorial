f8b36390aa4dfee2f75a11e2a0b6690a24d2ef1b
alexk101 <akiefer3@utk.edu>
2025-03-13 04:50:44 -0400

    ip directly

--- export_DDP_vars.sh
+++ export_DDP_vars.sh
@@ -1,4 +1,6 @@
 export RANK=$SLURM_PROCID
 export LOCAL_RANK=$SLURM_LOCALID
 export WORLD_SIZE=$SLURM_NTASKS
-export MASTER_ADDR=$(scontrol show hostname $SLURM_JOB_NODELIST | head -n 1)
+# export MASTER_ADDR=$(scontrol show hostname $SLURM_JOB_NODELIST | head -n 1)
+# Try using the IP address directly instead of hostname
+export MASTER_ADDR=$(scontrol show hostname $SLURM_JOB_NODELIST | head -n 1 | xargs getent hosts | awk '{print $1}')
