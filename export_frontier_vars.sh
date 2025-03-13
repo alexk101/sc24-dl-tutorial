ecf5656196c45f76e8e9b5b3f9f8997c587b1447
alexk101 <akiefer3@utk.edu>
2025-03-13 01:12:47 -0400

    tuning params

--- export_frontier_vars.sh
+++ export_frontier_vars.sh
@@ -1,7 +1,7 @@
 # Add these environment variables before source export_DDP_vars.sh
 export NCCL_DEBUG=INFO # For debugging network issues
 # export NCCL_DEBUG=WARN # For debugging network issues, less verbose
-export NCCL_SOCKET_IFNAME=hsn0
+export NCCL_SOCKET_IFNAME=hsn0,hsn1
 export NCCL_SOCKET_FAMILY=ipv4
 
 # Use job-specific temporary directory for NCCL
@@ -10,3 +10,4 @@ mkdir -p $NCCL_TEMP_DIR
 
 export NCCL_DMABUF_ENABLE=1
 export NCCL_NET_GDR_LEVEL=3
+export NCCL_ALGO=Tree
