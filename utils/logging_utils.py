4c690286b9684cad73e2cd6652812e062da0c004
alexk101 <akiefer3@utk.edu>
2025-03-13 22:31:44 -0400

    correct logging

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -2,31 +2,26 @@ import os
 import logging
 import sys
 from pathlib import Path
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - Rank %(rank)s - %(message)s',
-    handlers=[
-        logging.StreamHandler(sys.stdout),
-        logging.FileHandler(
-          str(
-            Path(__file__).parent / f"logs_{os.environ.get('SLURM_JOB_ID', '?')}"/ f"dist_test_rank{os.environ.get('SLURM_PROCID', '?')}.log"
-            )
-          )
-    ]
-)
 # Custom logger that includes rank
 class RankLogger:
-    def __init__(self, rank):
-        self.rank = rank
-        self.logger = logging.getLogger()
-        rank_log_dir = Path(__file__).parent / f"logs_{os.environ.get('SLURM_JOB_ID', '?')}"
-        rank_log_dir.mkdir(parents=True, exist_ok=True)
+  def __init__(self, rank):
+    self.rank = rank
+    self.logger = logging.getLogger()
+    rank_log_dir = Path(__file__).parent.parent / f"logs_{os.environ.get('SLURM_JOB_ID', '?')}"
+    rank_log_dir.mkdir(parents=True, exist_ok=True)
+    logging.basicConfig(
+      level=logging.INFO,
+      format='%(asctime)s - Rank %(rank)s - %(message)s',
+      handlers=[
+        logging.StreamHandler(sys.stdout),
+        logging.FileHandler(str(rank_log_dir / f"dist_test_rank{os.environ.get('SLURM_PROCID', '?')}.log"))
+      ]
+    )
         
-    def info(self, msg):
-        self.logger.info(msg, extra={'rank': self.rank})
-        
-    def error(self, msg):
-        self.logger.error(msg, extra={'rank': self.rank})
+  def info(self, msg):
+      self.logger.info(msg, extra={'rank': self.rank})
+      
+  def error(self, msg):
+      self.logger.error(msg, extra={'rank': self.rank})
 
 GLOBAL_LOG = RankLogger(os.environ.get("SLURM_PROCID", "?"))
