9f417aa8e7233614b2955f2c58e799e8a63e299b
Alex Kiefer <alexkiefer51@gmail.com>
2025-03-16 10:32:00 -0500

    that didnt work

--- utils/comm.py
+++ utils/comm.py
@@ -145,12 +145,10 @@ def init_process_group_perl():
 def init_process_group_mpi():
     """Initialize the process group using MPI"""
     num_gpus_per_node = torch.cuda.device_count()
-    # comm = MPI.COMM_WORLD
-    # world_size = comm.Get_size()
-    # world_rank = comm.Get_rank()
-    # local_rank = int(global_rank) % int(num_gpus_per_node)
-    world_size = int(os.getenv("WORLD_SIZE", 1))
-    world_rank = int(os.getenv("RANK", 0))
+    comm = MPI.COMM_WORLD
+    world_size = comm.Get_size()
+    global_rank = comm.Get_rank()
+    local_rank = int(global_rank) % int(num_gpus_per_node) 
 
 
     if world_size > 1:
@@ -159,7 +157,7 @@ def init_process_group_mpi():
                 backend="nccl",
                 #init_method="tcp://{}:{}".format(args.master_addr, args.master_port),
                 init_method='env://',
-                rank=world_rank,
+                rank=global_rank,
                 world_size=world_size,
             )
 
