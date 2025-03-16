7a3763f7fecc8fe935c506f6ed5dccccdf9b45f5
Alex Kiefer <alexkiefer51@gmail.com>
2025-03-16 10:07:36 -0500

    added more network interfaces

--- export_frontier_vars.sh
+++ export_frontier_vars.sh
@@ -1,12 +1,12 @@
 # Add these environment variables before source export_DDP_vars.sh
 # export NCCL_DEBUG=INFO # For debugging network issues
 # export NCCL_DEBUG=WARN # For debugging network issues, less verbose
-export NCCL_SOCKET_IFNAME=hsn0
+export NCCL_SOCKET_IFNAME=hsn0,hsn1,hsn2,hsn3
 export NCCL_SOCKET_FAMILY=ipv4
 export MPICH_GPU_SUPPORT_ENABLED=1
 export NCCL_CROSS_NIC=1       # On large systems, this NCCL setting has been found to improve perf
 export PMI_NO_FORK=1
-export NCCL_IB_HCA=hsn0
+export NCCL_IB_HCA=hsn0,hsn1,hsn2,hsn3
 
 # # Alternative approach to isolate NCCL communications
 # export TORCH_NCCL_BLOCKING_WAIT=1
