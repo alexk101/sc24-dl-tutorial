27b0fa1716aff405d39607525a7cff52f8c5fc77
Alex Kiefer <alexkiefer51@gmail.com>
2025-03-19 18:58:12 +0100

    move import after env var declaration

--- gen_data.py
+++ gen_data.py
@@ -1,8 +1,9 @@
-from utils.data import data_subset
 import os
 
 os.environ["DATADIR"] = "/lustre/orion/geo163/proj-shared/downsampled_data"
 os.environ["SCRATCH"] = "/lustre/orion/geo163/scratch/kiefera"
+
+from utils.data import data_subset
 
 subsets = [1, 5, 10, 15, 20, 25]
 for subset in subsets:
