2a8ee4af3572539e04a9f5c8111f152a80fa8f08
Alex Kiefer <alexkiefer51@gmail.com>
2025-03-19 18:55:33 +0100

    use correct env var

--- gen_data.py
+++ gen_data.py
@@ -1,7 +1,7 @@
 from utils.data import data_subset
 import os
 
-os.environ["DATADIR"] = "/lustre/orion/geo163/proj-shared/downsampled_data"
+os.environ["DATA_DIR"] = "/lustre/orion/geo163/proj-shared/downsampled_data"
 os.environ["SCRATCH"] = "/lustre/orion/geo163/scratch/kiefera"
 
 subsets = [1, 5, 10, 15, 20, 25]
