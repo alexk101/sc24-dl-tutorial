8f7a9b2ae1700e4e1330bb95cb1e45efae14c484
Alex Kiefer <alexkiefer51@gmail.com>
2025-03-19 18:46:41 +0100

    no cleanup

--- train_mp_mod.py
+++ train_mp_mod.py
@@ -773,7 +773,6 @@ if __name__ == "__main__":
         params.experiment_dir = os.path.abspath(expDir)
 
         # Setup data
-        clean_up_temp_dirs(params.n_train)
         data_subset(params.n_train)
         params.train_data_path = str(TEMP_TRAIN/str(params.n_train))
         params.valid_data_path = str(TEMP_VAL/str(params.n_train))
@@ -804,5 +803,3 @@ if __name__ == "__main__":
     if params.distributed:
         torch.distributed.barrier()
     logging.info("DONE ---- rank %d" % world_rank)
-    if world_rank == 0:
-        clean_up_temp_dirs(params.n_train)
