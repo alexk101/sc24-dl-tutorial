afc10bb8353316f34ad42fede4567ee5a8ae3ed4
Alex Kiefer <alexkiefer51@gmail.com>
2025-03-31 19:05:10 -0400

    correct logging

--- train_mp_mod.py
+++ train_mp_mod.py
@@ -184,9 +184,9 @@ def train(params, args, local_rank, world_rank, world_size, hyperparameter_searc
 
     if params.enable_jit:
         torch.set_float32_matmul_precision('high')
-        # Enable detailed debugging
-        torch._dynamo.config.verbose = True
-        torch._dynamo.config.log_level = logging.DEBUG
+        # Enable detailed debugging for TorchDynamo
+        import torch._logging
+        torch._logging.set_logs(dynamo=logging.DEBUG, graph_breaks=True)
         model = torch.compile(model)
 
     if params.gradient_checkpointing:
