e241ac008e8c010c60f43627d3d45014468dfac6
Alex Kiefer <alexkiefer51@gmail.com>
2025-03-31 17:18:28 -0400

    perhaps the order matters

--- submit_frontier.sh
+++ submit_frontier.sh
@@ -42,9 +42,9 @@ export LD_LIBRARY_PATH=/ccs/home/kiefera/scratch/rccl/aws-ofi-rccl/lib:$LD_LIBRA
 
 # Needed to bypass MIOpen, Disk I/O Errors
 export MIOPEN_USER_DB_PATH="/tmp/my-miopen-cache-$SLURM_JOB_ID"
-mkdir -p ${MIOPEN_USER_DB_PATH}
 export MIOPEN_CUSTOM_CACHE_DIR=${MIOPEN_USER_DB_PATH}
-export MIOPEN_DISABLE_CACHE=1
+rm -rf ${MIOPEN_USER_DB_PATH}
+mkdir -p ${MIOPEN_USER_DB_PATH}
 
 LOGDIR=${SCRATCH}/sc24-dl-tutorial/logs
 mkdir -p ${LOGDIR}
